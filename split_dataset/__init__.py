3019f1db649403035ef1d0e4a2dd9e46122f2029
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-05-06 17:41:54 +0200

    Bump version: 0.1.1 → 0.1.2

--- split_dataset/__init__.py
+++ split_dataset/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for Split Dataset."""
 
 __author__ = """Vilim Stih & Luigi Petrucco @portugueslab"""
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from split_dataset.blocks import Blocks
