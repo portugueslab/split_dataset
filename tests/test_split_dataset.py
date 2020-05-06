8dee04416ad8aed677ce2864febc6a8e44f54f7e
vilim <vilim@neuro.mpg.de>
2020-05-06 17:46:49 +0200

    Added the actual split datasets

--- tests/test_split_dataset.py
+++ tests/test_split_dataset.py
@@ -2,20 +2,56 @@
 
 """Tests for `split_dataset` package."""
 
-import pytest
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+import unittest
+import shutil
+import numpy as np
+import tempfile
+from split_dataset import save_to_split_dataset
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+class TestSplitDataset(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_SplitDataset(self):
+        dims = [(10, 3, 3, 3), (5, 5, 5), (5, 5), (1, 5, 5, 5)]
+        block_sizes = [(2, None, None, None), (1, None, 3), (2, None), (None, 2, 5, 5)]
+        all_slices = [
+            [(slice(3, 8), slice(None))],
+            [(slice(0, 1),), (slice(0, 2), slice(0, 1), slice(None))],
+            [slice(0, 2)],
+            [
+                (slice(0, 1),),
+                (slice(0, 2), slice(0, 1), slice(None)),
+                (0, slice(0, 2), slice(0, 1)),
+            ],
+        ]
+
+        for i, (di, bs, slices) in enumerate(zip(dims, block_sizes, all_slices)):
+            test_data = np.arange(np.product(di)).reshape(di)
+
+            sd = save_to_split_dataset(
+                test_data,
+                block_size=bs,
+                root_name=self.test_dir,
+                prefix="te{:02d}".format(i),
+            )
+            for sl in slices:
+                a = sd[sl]
+                b = test_data[sl]
+                np.testing.assert_equal(
+                    a,
+                    b,
+                    err_msg="Testing "
+                    + str(di)
+                    + " "
+                    + str(sl)
+                    + " of shape "
+                    + str(a.shape)
+                    + " and shape"
+                    + str(b.shape),
+                )
 
