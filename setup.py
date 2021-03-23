69d9a2096eb72403ad5d0cc245593e57729486ad
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-03-23 01:23:00 +0100

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     packages=find_packages(include=["split_dataset", "split_dataset.*"]),
     test_suite="tests",
     url="https://github.com/portugueslab/split_dataset",
-    version="0.3.0",
+    version="version='0.4.0'",
     zip_safe=False,
 )
