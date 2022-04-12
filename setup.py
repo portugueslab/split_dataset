2ab22f2bb454e7781ed62e681dff531740d3ee8f
ykwu <ykure.w@gmail.com>
2022-04-12 16:08:34 +0200

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     packages=find_packages(include=["split_dataset", "split_dataset.*"]),
     test_suite="tests",
     url="https://github.com/portugueslab/split_dataset",
-    version="0.4.2",
+    version="0.4.3",
     zip_safe=False,
 )
